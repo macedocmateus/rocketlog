38423755b13ac264fe6593bc160a7584ed49814c
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-24 19:36:31 -0300

    add zod for validation treatment

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "@/utils/AppError";
+import { ZodError } from "zod";
 
 export function errorHandling(
     error: any,
@@ -7,11 +8,19 @@ export function errorHandling(
     response: Response,
     next: NextFunction,
 ) {
-    // Erro lançado pelo dev, com o código do status e a mensagem personalizada
+    // Erro lançado pelo dev, com o código do status e a mensagem personalizada, Para tratamento de exceções
     if (error instanceof AppError) {
         return response
             .status(error.statusCode)
             .json({ message: error.message });
+    }
+
+    // Tratamento de erros de validação
+    if (error instanceof ZodError) {
+        return response.status(400).json({
+            message: "validation error",
+            issues: error.format(),
+        });
     }
 
     // Erro genérico
