f1de009ae551f8489da1b7c8bd6be927563eabf1
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-26 16:42:01 -0300

    fix bug in prisma client import into database prisma

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { z } from "zod";
 import { hash } from "bcrypt";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 
 class UsersController {
     async create(request: Request, response: Response, next: NextFunction) {
@@ -12,12 +14,27 @@ class UsersController {
 
         const { name, email, password } = bodySchema.parse(request.body);
 
+        // Verificando se o email já existe e retorna uma exceção se caso exista um email igual
+        const userWithSameEmail = await prisma.user.findFirst({
+            where: { email },
+        });
+
+        if (userWithSameEmail) {
+            throw new AppError("User with same email already exists");
+        }
+
         const hashedPassword = await hash(password, 8);
 
-        return response.status(201).json({
-            message: "ok",
-            hashedPassword,
+        // Criando um novo usuário usando a ideia de orm do prisma e salvando no banco de dados
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword,
+            },
         });
+
+        return response.status(201).json(user);
     }
 }
 
--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient({
     // A fila de log só serão exibidas se caso ambiente for de desenvolvimento
