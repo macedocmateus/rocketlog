3c369c15468873c36b29d3542f24b44df74a5865
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-27 17:55:52 -0300

    add sessions controller and routes

---
+++ src/controllers/sessions-controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+class SessionsController {
+    create(request: Request, response: Response) {
+        return response.json({ message: "ok" });
+    }
+}
+
+export { SessionsController };
