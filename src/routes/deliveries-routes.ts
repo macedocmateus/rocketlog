6fbc7c9e94fa35a0fa41c197205289d10a965c31
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-02 20:24:07 -0300

    add deliveries-status for change status response using http patch for update it

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
+
 import { DeliveriesController } from "@/controllers/deliveries-controller";
+import { DeliveriesStatusController } from "@/controllers/deliveries-status-controller";
+
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
+const deliveriesStatusController = new DeliveriesStatusController();
 
 // Verificando se o usuário está autenticado e se ele tem permissão
 deliveriesRoutes.use(
@@ -14,5 +18,7 @@ deliveriesRoutes.use(
 
 deliveriesRoutes.post("/", deliveriesController.create);
 deliveriesRoutes.get("/", deliveriesController.index);
+
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update);
 
 export { deliveriesRoutes };
